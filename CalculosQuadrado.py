6ef131a77c54dca280baab26f7b67ea73e54ce1f
biroska <Biroska@gmail.com>
2018-05-26 13:42:36 +0000

    Inclusao de programa que calcula o perimetro e area de um quadrado

---
+++ CalculosQuadrado.py
@@ -0,0 +1,12 @@
+# Este programa contempla a resolução do primeiro exercício do curso:
+#     Introdução à Ciência da Computação com Python Parte 1 
+# Disponível no coursera.
+
+#                                EXERCÍCIO 1 
+# Faça um programa em Python que receba (entrada de dados) o valor correspondente ao 
+# lado de um quadrado, calcule e imprima (saída de dados) seu perímetro e sua área.
+
+
+lado = input("Digite o valor correspondente ao lado de um quadrado: ")
+
+print("perímetro: ", 4*int(lado)," - área: ",int(lado)**2)
